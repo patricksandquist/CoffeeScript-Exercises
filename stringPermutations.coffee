7656ee59af15c79d3cc22dc41a47a2edc3151eab
patricksandquist <patricksandquist@gmail.com>
2016-01-21 17:34:25 -0800

    Clean up

--- stringPermutations.coffee
+++ stringPermutations.coffee
@@ -19,25 +19,3 @@ permutations = (str) ->
   output
 
 console.log(permutations "test")
-
-###
-For reference, here is the original Javascript:
-
-var permutations = function (str) {
-  if (str.length < 2) {
-    return [str];
-  }
-
-  var letter = str[0];
-  var lower_perms = permutations(str.slice(1));
-  var output = [];
-
-  lower_perms.forEach(function (perm) {
-    for (var i = 0; i <= perm.length; i++) {
-      output.push(perm.slice(0, i) + letter + perm.slice(i));
-    }
-  });
-
-  return output;
-};
-###
--- stringPermutations.js
+++ stringPermutations.js
@@ -29,27 +29,4 @@ Generate all permutations of a given string.
 
   console.log(permutations("test"));
 
-
-  /*
-  For reference, here is the original Javascript:
-  
-  var permutations = function (str) {
-    if (str.length < 2) {
-      return [str];
-    }
-  
-    var letter = str[0];
-    var lower_perms = permutations(str.slice(1));
-    var output = [];
-  
-    lower_perms.forEach(function (perm) {
-      for (var i = 0; i <= perm.length; i++) {
-        output.push(perm.slice(0, i) + letter + perm.slice(i));
-      }
-    });
-  
-    return output;
-  };
-   */
-
 }).call(this);
